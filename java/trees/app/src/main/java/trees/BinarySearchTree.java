38fdea7b9962fa6bc8d872723e0624fa339100cf
OdehYazan <eng.yazanodeh@gmail.com>
2021-10-16 17:22:35 +0300

    ch15

---
+++ java/trees/app/src/main/java/trees/BinarySearchTree.java
@@ -0,0 +1,105 @@
+package trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinarySearchTree<T extends Comparable<T>> {
+
+  private Node<T> root;
+  private Node<T> leftNode;
+  private Node<T> rightNode;
+
+  public void insert(T data) {
+    if (isEmpty()) { // tree empty
+      root = new Node<>(data);
+    } else {
+      insertHelper(data, root);
+    }
+  }
+
+  public void inorderTraversal() {
+    if (isEmpty()) {
+      return;
+    }
+
+    traverseInorder(root);
+  }
+
+  private void traverseInorder(Node<T> root) {
+    if (root.getLeftNode() != null) { // traverse left
+      traverseInorder(root.getLeftNode());
+    }
+
+    // visit / print the root of the tree (root could be a sub-tree)
+    System.out.print(root.getData() + " -> ");
+
+    if (root.getRightNode() != null) { // traverse right
+      traverseInorder(root.getRightNode());
+    }
+  }
+
+
+
+  public void preorderTraversal() {
+    if (isEmpty()) {
+      return;
+    }
+
+    preOrder(root);
+  }
+  public void preOrder( Node<T> node) {
+    if (node == null) {
+      return;
+    }
+
+    System.out.printf( node.getData() +" -> ");
+    preOrder(node.getLeftNode());
+    preOrder(node.getRightNode());
+  }
+
+
+  public void postorderTraversal() {
+    if (isEmpty()) {
+      return;
+    }
+
+    postOrder(root);
+  }
+  public void postOrder( Node<T> node) {
+    if (node == null) {
+      return;
+    }
+
+
+    preOrder(node.getLeftNode());
+    preOrder(node.getRightNode());
+    System.out.printf( node.getData() +" -> ");
+  }
+
+
+
+  private void insertHelper(T data, Node<T> root) {
+    Node<T> node = new Node<>(data);
+    if (data.compareTo(root.getData()) < 0) {
+      if (root.getLeftNode() == null) {
+        root.setLeftNode(node);
+      } else {
+        insertHelper(data, root.getLeftNode());
+      }
+    } else if(data.compareTo(root.getData()) > 0) {
+      if (root.getRightNode() == null) {
+        root.setRightNode(node);
+      } else {
+        insertHelper(data, root.getRightNode());
+      }
+    }
+  }
+
+
+
+
+
+  public boolean isEmpty() {
+    return root == null;
+  }
+}
